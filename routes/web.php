7dcac8b2ac823add69488c7427ac407f8791bdde
azhagiri <azhagirishankar5@gmail.com>
2024-07-29 16:19:36 +0530

    #1 Refactor layout and Database , added Auth user login and register.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'home']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Auth::routes();
+require __DIR__.'/auth.php';
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::prefix('admin')->group(function () {
-   Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
-});
+Route::get('admin/dashboard', [HomeController::class, 'index'])->name('home')
+     ->middleware(['auth','admin']);
